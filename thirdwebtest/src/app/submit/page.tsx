c592e8549cb01c5ac56dc18b0d94e0576607c2a4
ercole89 <davide.ercole89@gmail.com>
2024-07-10 16:10:40 +0200

    errore build

--- thirdwebtest/src/app/submit/page.tsx
+++ thirdwebtest/src/app/submit/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useState } from 'react';
 import { useActiveAccount, TransactionButton, ThirdwebProvider } from 'thirdweb/react';
 import { createThirdwebClient, defineChain, getContract, prepareContractCall, prepareTransaction, toWei } from 'thirdweb';
