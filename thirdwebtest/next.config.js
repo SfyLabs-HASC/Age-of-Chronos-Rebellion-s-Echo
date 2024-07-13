89e6df9606f7776443ca9278a430c017f5eb922f
ercole89 <davide.ercole89@gmail.com>
2024-07-13 17:07:27 +0200

    solo http

--- thirdwebtest/next.config.js
+++ thirdwebtest/next.config.js
@@ -1,4 +1,32 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    async headers() {
+      return [
+        {
+          // Applica le intestazioni di sicurezza a tutte le rotte
+          source: '/(.*)',
+          headers: [
+            {
+              key: 'Strict-Transport-Security',
+              value: 'max-age=63072000; includeSubDomains; preload'
+            },
+            {
+              key: 'X-Frame-Options',
+              value: 'SAMEORIGIN'
+            },
+            {
+              key: 'X-Content-Type-Options',
+              value: 'nosniff'
+            },
+            {
+              key: 'Referrer-Policy',
+              value: 'origin-when-cross-origin'
+            }
+          ],
+        },
+      ]
+    },
+  }
+  
+  module.exports = nextConfig
+  
