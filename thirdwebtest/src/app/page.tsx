e694e52107cbb321f1aeafc95b412cd9eb7f4f21
ercole89 <davide.ercole89@gmail.com>
2024-07-14 21:03:56 +0200

    FINAL BEFORE THE REBELLION

--- thirdwebtest/src/app/page.tsx
+++ thirdwebtest/src/app/page.tsx
@@ -19,9 +19,6 @@ export default function Home() {
           <Link href="./mint" passHref className="hex_button turret-road-bold">
             JOIN THE REBELLION
           </Link>
-          <Link href="./game" passHref className="hex_button turret-road-bold">
-            PLAY GAME
-          </Link>
           
           <ul className="socialLink d-flex flex-row justify-content-center m-0 p-0">
             <li>
