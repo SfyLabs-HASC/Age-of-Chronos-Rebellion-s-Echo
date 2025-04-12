a86c18c4f8153afb5bd2c60bd7bd48780c55e16c
Daniele <damonaco@unisa.it>
2025-04-12 17:23:37 +0200

    Added cors policy Integrated new blockchain pipeline

--- thirdwebtest/src/app/api/davide/route.ts
+++ thirdwebtest/src/app/api/davide/route.ts
@@ -24,8 +24,9 @@ const backendWalletAddress = '0x93e7b1f3fA8f57425B8a80337D94Ae3992879911';
 //const chain = '1287';  //moonbasealpha
 
 //productino
-const managerContractAddress = '0xd50248022D8b254De8923109664918f707e4D074';
+const managerContractAddress = '0xD5C5D74C47dd879109c63134F3bA74ACf79543AC';
 const chain = '1284';  //moonbeam
+const targetTopic = "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef";
 
 
 export async function GET(req: NextRequest) {
@@ -42,7 +43,7 @@ export async function POST(req: NextRequest) {
   try {
     const data = await req.json();
     //console.log("Data received in POST request:", data);
-    const { solidityFunction, rykerTokenId, lunaTokenId, ariaTokenId, thaddeusTokenId, whichChild, key } = data;
+    const { solidityFunction, rykerTokenId, lunaTokenId, ariaTokenId, thaddeusTokenId, gameLevel, key } = data;
 
     // Controlli sui parametri ricevuti
     if (solidityFunction !== 'startMission' && solidityFunction !== 'endMission') {
@@ -54,23 +55,22 @@ export async function POST(req: NextRequest) {
       bodyToHash = {
         functionName: solidityFunction,
         args: [
+          gameLevel,
           rykerTokenId,
           lunaTokenId,
           ariaTokenId,
-          thaddeusTokenId,
-          key
+          thaddeusTokenId
         ]
       };
     } else if (solidityFunction === 'endMission') {
       bodyToHash = {
         functionName: solidityFunction,
         args: [
+          gameLevel,
           rykerTokenId,
           lunaTokenId,
           ariaTokenId,
-          thaddeusTokenId,
-          whichChild,
-          key
+          thaddeusTokenId
         ]
       };
     }
@@ -96,7 +96,7 @@ export async function POST(req: NextRequest) {
         requestBody,
         {
           headers: {
-            'simulateTx':'true',
+            'simulateTx':'false',
             'Content-Type': 'application/json',
             'Authorization': `Bearer ${accessToken}`,
             'x-backend-wallet-address': backendWalletAddress,
@@ -105,7 +105,77 @@ export async function POST(req: NextRequest) {
         }
       );
 
-      //console.log("Response:", response.data);
+      if(solidityFunction == "endMission")
+      {
+        const maxAttempts = 18;
+        const pollIntervalMs = 10 * 1000;
+        
+        let statusResp : any;
+        for (let attempt = 0; attempt < maxAttempts; attempt++) {
+          statusResp = await axios.get(
+            `${engineBaseUrl}/transaction/status/${response.data.result.queueId}`,
+            {
+              headers: {
+                Authorization: `Bearer ${accessToken}`,
+              },
+            },
+          );
+          console.log("Transaction status: " + JSON.stringify(statusResp.data, null, 2))
+          // status can be: processed, queued, sent, errored, mined, cancelled, retried
+          const isComplete = statusResp.data.result.status === "mined";
+
+          if(isComplete)
+          {
+            console.log("Transaction complete")
+            break;
+          }
+
+          const isError = ["errored", "cancelled"].includes(
+            statusResp.data.result.status,
+          );
+
+          if(isError)
+          {
+            console.log("Transaction error")
+            return NextResponse.json({ error: JSON.stringify(statusResp.data, null, 2) }, { status: 500 });
+          }
+
+          
+          await new Promise((resolve) => setTimeout(resolve, pollIntervalMs));
+        }
+
+          const txResp = await axios.get(
+          `${engineBaseUrl}/transaction/${chain}/tx-hash/${statusResp.data.result.transactionHash}`,
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          },
+        );
+
+        const txRespData = txResp.data;
+        console.log("Transaction status: " + JSON.stringify(txRespData, null, 2))
+
+        const matchingLog = txResp.data.result.logs.find((log: any) => log.topics[0].toLowerCase() === targetTopic);
+        
+        if (matchingLog && matchingLog.topics.length > 3) {
+          const minedTokenId = matchingLog.topics[3];
+          console.log("The 4th topic is:", minedTokenId);
+          const tokenIdDec = parseInt(minedTokenId, 16);
+          const mintResp = {
+            childCollection: matchingLog.address,
+            tokenId: tokenIdDec
+          };
+          return NextResponse.json({ result: mintResp }, { status: 201 });
+        } else {
+          console.log("Token Not found.");
+          return NextResponse.json({ error: "Token not found " + matchingLog }, { status: 500 });
+        }
+      }
+      
+
+      console.log("Body: ", bodyToHash);
+      console.log("Response: ", response.data);
       return NextResponse.json({ result: response.data }, { status: 201 });
     } catch (error: any) {
       if (error.response) {
