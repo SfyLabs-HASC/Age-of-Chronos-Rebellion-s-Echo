9313e142ccdb9a79bc10262edefacbf3eb2b00a8
Daniele <daniele@picaresquestudio.com>
2025-05-20 16:03:01 +0200

    Added redirect to avoid entering the game through address without www in the beginning of the url

--- thirdwebtest/next.config.js
+++ thirdwebtest/next.config.js
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async redirects() {
+    return [
+      {
+        source: '/(.*)',
+        has: [{ type: 'host', value: 'ageofchronos.com' }],
+        permanent: true,
+        destination: 'https://www.ageofchronos.com/:1',
+      },
+    ]
+    },
+
     async headers() {
         return [
             {
@@ -7,7 +18,7 @@ const nextConfig = {
                 source: "/api/:path*",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+                    { key: "Access-Control-Allow-Origin", value: 'https://www.ageofchronos.com' }, // replace this your actual origin
                     { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
                     { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
                 ]
